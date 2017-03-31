962c97d61fd5083de8c63919f7296f99f14a7267
karimata110611 <karimata1106@gmail.com>
2017-04-01 00:25:59 +0900

    001
    MVVMのみ

---
+++ YouTube/001.YouTubeDemo (MVVM化のみ)/YouTubeDemo/YouTubeDemo/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using YouTubeDemo.Views;
+
+namespace Views.YouTubeDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new YouTubeViewPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
